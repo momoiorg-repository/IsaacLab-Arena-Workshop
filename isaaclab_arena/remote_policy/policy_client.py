isaac-sim/IsaacLab-Arena

add socket code for policy

isaaclab_arena/remote_policy/policy_client.py
@@ -1,0 +1,94 @@
+# Copyright (c) 2025, The Isaac Lab Arena Project Developers (https://github.com/isaac-sim/IsaacLab-Arena/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+import zmq
+
+from .message_serializer import MessageSerializer
+from .utils import RemotePolicyConfig 
+
+class PolicyClient:
+    """Synchronous client for talking to a PolicyServer over ZeroMQ."""
+
+    def __init__(self, config: RemotePolicyConfig) -> None:
+        self._config = config
+        self._context = zmq.Context()
+        self._socket = self._context.socket(zmq.REQ)
+        self._socket.setsockopt(zmq.RCVTIMEO, self._config.timeout_ms)
+        self._socket.connect(f"tcp://{self._config.host}:{self._config.port}")
+
+    # ------------------------------------------------------------------ #
+    # Public API
+    # ------------------------------------------------------------------ #
+
+    def ping(self) -> bool:
+        """Check if the server is reachable."""
+        try:
+            self.call_endpoint("ping", requires_input=False)
+            return True
+        except Exception:
+            return False
+
+    def reset(self, env_ids=None, options: Optional[Dict[str, Any]] = None) -> Any:
+        """Reset remote policy state."""
+        return self.call_endpoint(
+            endpoint="reset",
+            data={"env_ids": env_ids, "options": options},
+            requires_input=True,
+        )
+
+    def kill(self) -> Any:
+        """Ask remote server to stop main loop."""
+        return self.call_endpoint("kill", requires_input=False)
+
+    def get_action(
+        self,
+        observation: Dict[str, Any],
+        options: Optional[Dict[str, Any]] = None,
+    ) -> Dict[str, Any]:
+        """Send policy_observations and get back policy action dict."""
+        payload: Dict[str, Any] = {"observation": observation}
+        # if options is not None:
+        #     payload["options"] = options
+    
+        resp = self.call_endpoint(
+            endpoint="get_action",
+            data=payload,
+            requires_input=True,
+        )
+        if isinstance(resp, dict) and "action" in resp:
+            return resp["action"]
+        return resp
+
+    def call_endpoint(
+        self,
+        endpoint: str,
+        data: Optional[Dict[str, Any]] = None,
+        requires_input: bool = True,
+    ) -> Any:
+        """Generic RPC helper."""
+        request: Dict[str, Any] = {"endpoint": endpoint}
+        if requires_input:
+            request["data"] = data or {}
+        if self._config.api_token:
+            request["api_token"] = self._config.api_token
+
+        self._socket.send(MessageSerializer.to_bytes(request))
+        message = self._socket.recv()
+        response = MessageSerializer.from_bytes(message)
+
+        if isinstance(response, dict) and "error" in response:
+            raise RuntimeError(f"Server error: {response['error']}")
+        return response
+
+    def close(self) -> None:
+        """Close the underlying ZeroMQ socket and context."""
+        self._socket.close()
+        self._context.term()
+

isaaclab_arena_gr00t/gr00t_remote_policy.py
@@ -1,0 +1,95 @@
+# Copyright (c) 2025, The Isaac Lab Arena Project Developers (https://github.com/isaac-sim/IsaacLab-Arena/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Any, Dict, Tuple
+
+from gr00t.experiment.data_config import DATA_CONFIG_MAP, load_data_config
+from gr00t.model.policy import Gr00tPolicy
+
+from isaaclab_arena.remote_policy.model_policy import ModelPolicy
+from isaaclab_arena_gr00t.policy_config import Gr00tClosedloopPolicyConfig
+from isaaclab_arena_gr00t.data_utils.io_utils import create_config_from_yaml
+
+
+class Gr00tRemoteModelPolicy(ModelPolicy):
+    """Server-side wrapper around Gr00tPolicy."""
+
+    def __init__(self, policy_config_yaml_path: Path) -> None:
+        print(f"[Gr00tRemoteModelPolicy] loading config from: {policy_config_yaml_path}")
+        self._cfg = create_config_from_yaml(policy_config_yaml_path, Gr00tClosedloopPolicyConfig)
+        print(
+            "[Gr00tRemoteModelPolicy] config:\n"
+            f"  model_path        = {self._cfg.model_path}\n"
+            f"  embodiment_tag    = {self._cfg.embodiment_tag}\n"
+            f"  task_mode_name    = {self._cfg.task_mode_name}\n"
+            f"  data_config       = {self._cfg.data_config}\n"
+            f"  action_horizon    = {self._cfg.action_horizon}\n"
+            f"  action_chunk_len  = {self._cfg.action_chunk_length}\n"
+            f"  pov_cam_name_sim  = {self._cfg.pov_cam_name_sim}\n"
+            f"  policy_device     = {self._cfg.policy_device}"
+        )
+        self._policy = self._load_gr00t_policy()
+        print("[Gr00tRemoteModelPolicy] Gr00tPolicy loaded successfully")
+
+    def _load_gr00t_policy(self) -> Gr00tPolicy:
+        print(f"[Gr00tRemoteModelPolicy] loading data_config={self._cfg.data_config}")
+        if self._cfg.data_config in DATA_CONFIG_MAP:
+            data_config = DATA_CONFIG_MAP[self._cfg.data_config]
+        elif self._cfg.data_config == "unitree_g1_sim_wbc":
+            data_config = load_data_config("isaaclab_arena_gr00t.data_config:UnitreeG1SimWBCDataConfig")
+        else:
+            raise ValueError(f"Invalid data config: {self._cfg.data_config}")
+
+        modality_config = data_config.modality_config()
+        modality_transform = data_config.transform()
+
+        model_path = Path(self._cfg.model_path)
+        if not model_path.exists():
+            raise FileNotFoundError(f"Model path does not exist: {model_path}")
+        print(f"[Gr00tRemoteModelPolicy] loading checkpoint from: {model_path}")
+
+        policy = Gr00tPolicy(
+            model_path=str(model_path),
+            modality_config=modality_config,
+            modality_transform=modality_transform,
+            embodiment_tag=self._cfg.embodiment_tag,
+            denoising_steps=self._cfg.denoising_steps,
+            device=self._cfg.policy_device,
+        )
+        return policy
+
+    # ------------------------------------------------------------------ #
+    # ModelPolicy interface
+    # ------------------------------------------------------------------ #
+
+    def get_action(
+        self,
+        observation: Dict[str, Any],
+        options: Dict[str, Any] | None = None,
+    ) -> Tuple[Dict[str, Any], Dict[str, Any]]:
+        print("[Gr00tRemoteModelPolicy] get_action called")
+        print(f"  observation keys: {list(observation.keys())}")
+        if options is not None:
+            print(f"  options keys: {list(options.keys())}")
+
+        result = self._policy.get_action(observation)
+        # Gr00tPolicy.get_action usually returns a dict; wrap it with empty info.
+        if isinstance(result, tuple) and len(result) == 2:
+            action, info = result
+        else:
+            action, info = result, {}
+
+        print("[Gr00tRemoteModelPolicy] get_action done")
+        return action, info
+
+    def reset(self, options: Dict[str, Any] | None = None) -> Dict[str, Any]:
+        print(f"[Gr00tRemoteModelPolicy] reset called: options={options}")
+        if hasattr(self._policy, "reset"):
+            self._policy.reset(options=options)
+        return {}
+

momoiorg-repository/IsaacLab-Arena-Workshop

isaaclab_arena/remote_policy/policy_client.py
@@ -1,0 +1,93 @@
+# Copyright (c) 2025, The Isaac Lab Arena Project Developers (https://github.com/isaac-sim/IsaacLab-Arena/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+import zmq
+
+from .message_serializer import MessageSerializer
+from .remote_policy_config import RemotePolicyConfig 
+
+class PolicyClient:
+    """Synchronous client for talking to a PolicyServer over ZeroMQ."""
+
+    def __init__(self, config: RemotePolicyConfig) -> None:
+        self._config = config
+        self._context = zmq.Context()
+        self._socket = self._context.socket(zmq.REQ)
+        self._socket.setsockopt(zmq.RCVTIMEO, self._config.timeout_ms)
+        self._socket.connect(f"tcp://{self._config.host}:{self._config.port}")
+
+    # ------------------------------------------------------------------ #
+    # Public API
+    # ------------------------------------------------------------------ #
+
+    def ping(self) -> bool:
+        """Check if the server is reachable."""
+        try:
+            self.call_endpoint("ping", requires_input=False)
+            return True
+        except Exception:
+            warnings.warn(
+                f"[PolicyClient] Failed to ping remote policy server at "
+                f"{self._config.host}:{self._config.port}: {exc}"
+            )
+            return False
+
+    def reset(self, env_ids=None, options: Optional[Dict[str, Any]] = None) -> Any:
+        """Reset remote policy state."""
+        return self.call_endpoint(
+            endpoint="reset",
+            data={"env_ids": env_ids, "options": options},
+            requires_input=True,
+        )
+
+    def kill(self) -> Any:
+        """Ask remote server to stop main loop."""
+        return self.call_endpoint("kill", requires_input=False)
+
+    def get_action(
+        self,
+        observation: Dict[str, Any],
+    ) -> Dict[str, Any]:
+        """Send policy_observations and get back policy action dict."""
+        payload: Dict[str, Any] = {"observation": observation}
+    
+        resp = self.call_endpoint(
+            endpoint="get_action",
+            data=payload,
+            requires_input=True,
+        )
+        return resp
+
+    def call_endpoint(
+        self,
+        endpoint: str,
+        data: Optional[Dict[str, Any]] = None,
+        requires_input: bool = True,
+    ) -> Any:
+        """Generic RPC helper."""
+        request: Dict[str, Any] = {"endpoint": endpoint}
+        if requires_input:
+            request["data"] = data or {}
+        if self._config.api_token:
+            request["api_token"] = self._config.api_token
+
+        self._socket.send(MessageSerializer.to_bytes(request))
+        message = self._socket.recv()
+        response = MessageSerializer.from_bytes(message)
+
+        if isinstance(response, dict) and "error" in response:
+            raise RuntimeError(f"Server error: {response['error']}")
+        return response
+
+    def close(self) -> None:
+        """Close the underlying ZeroMQ socket and context."""
+        self._socket.close()
+        self._context.term()
+

isaaclab_arena_gr00t/gr00t_remote_policy.py
@@ -1,0 +1,95 @@
+# Copyright (c) 2025, The Isaac Lab Arena Project Developers (https://github.com/isaac-sim/IsaacLab-Arena/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Any, Dict, Tuple
+
+from gr00t.experiment.data_config import DATA_CONFIG_MAP, load_data_config
+from gr00t.model.policy import Gr00tPolicy
+
+from isaaclab_arena.remote_policy.server_side_policy import ServerSidePolicy
+from isaaclab_arena_gr00t.policy_config import Gr00tClosedloopPolicyConfig
+from isaaclab_arena_gr00t.data_utils.io_utils import create_config_from_yaml
+
+
+class Gr00tRemoteServerSidePolicy(ServerSidePolicy):
+    """Server-side wrapper around Gr00tPolicy."""
+
+    def __init__(self, policy_config_yaml_path: Path) -> None:
+        print(f"[Gr00tRemoteServerSidePolicy] loading config from: {policy_config_yaml_path}")
+        self._cfg = create_config_from_yaml(policy_config_yaml_path, Gr00tClosedloopPolicyConfig)
+        print(
+            "[Gr00tRemoteServerSidePolicy] config:\n"
+            f"  model_path        = {self._cfg.model_path}\n"
+            f"  embodiment_tag    = {self._cfg.embodiment_tag}\n"
+            f"  task_mode_name    = {self._cfg.task_mode_name}\n"
+            f"  data_config       = {self._cfg.data_config}\n"
+            f"  action_horizon    = {self._cfg.action_horizon}\n"
+            f"  action_chunk_len  = {self._cfg.action_chunk_length}\n"
+            f"  pov_cam_name_sim  = {self._cfg.pov_cam_name_sim}\n"
+            f"  policy_device     = {self._cfg.policy_device}"
+        )
+        self._policy = self._load_gr00t_policy()
+        print("[Gr00tRemoteServerSidePolicy] Gr00tPolicy loaded successfully")
+
+    def _load_gr00t_policy(self) -> Gr00tPolicy:
+        print(f"[Gr00tRemoteServerSidePolicy] loading data_config={self._cfg.data_config}")
+        if self._cfg.data_config in DATA_CONFIG_MAP:
+            data_config = DATA_CONFIG_MAP[self._cfg.data_config]
+        elif self._cfg.data_config == "unitree_g1_sim_wbc":
+            data_config = load_data_config("isaaclab_arena_gr00t.data_config:UnitreeG1SimWBCDataConfig")
+        else:
+            raise ValueError(f"Invalid data config: {self._cfg.data_config}")
+
+        modality_config = data_config.modality_config()
+        modality_transform = data_config.transform()
+
+        model_path = Path(self._cfg.model_path)
+        if not model_path.exists():
+            raise FileNotFoundError(f"Model path does not exist: {model_path}")
+        print(f"[Gr00tRemoteServerSidePolicy] loading checkpoint from: {model_path}")
+
+        policy = Gr00tPolicy(
+            model_path=str(model_path),
+            modality_config=modality_config,
+            modality_transform=modality_transform,
+            embodiment_tag=self._cfg.embodiment_tag,
+            denoising_steps=self._cfg.denoising_steps,
+            device=self._cfg.policy_device,
+        )
+        return policy
+
+    # ------------------------------------------------------------------ #
+    # ServerSidePolicy interface
+    # ------------------------------------------------------------------ #
+
+    def get_action(
+        self,
+        observation: Dict[str, Any],
+        options: Dict[str, Any] | None = None,
+    ) -> Tuple[Dict[str, Any], Dict[str, Any]]:
+        print("[Gr00tRemoteServerSidePolicy] get_action called")
+        print(f"  observation keys: {list(observation.keys())}")
+        if options is not None:
+            print(f"  options keys: {list(options.keys())}")
+
+        result = self._policy.get_action(observation)
+        # Gr00tPolicy.get_action usually returns a dict; wrap it with empty info.
+        if isinstance(result, tuple) and len(result) == 2:
+            action, info = result
+        else:
+            action, info = result, {}
+
+        print("[Gr00tRemoteServerSidePolicy] get_action done")
+        return action, info
+
+    def reset(self, options: Dict[str, Any] | None = None) -> Dict[str, Any]:
+        print(f"[Gr00tRemoteServerSidePolicy] reset called: options={options}")
+        if hasattr(self._policy, "reset"):
+            self._policy.reset(options=options)
+        return {}
+